acf723660a244a8757c9e00b7589a77b9ae3a7ba
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-09-21 15:59:01 -0700

    Add error counting code
    It's halfway there or so

--- errors.js
+++ errors.js
@@ -1,3 +1,53 @@
 const fs = require('fs')
 const sh = require('shelljs')
-const ts = require('typescript')
+/** @typedef {{ date: string, number: number, sha: string, parentSha: string }} Pr */
+
+/** @param {string} commit */
+function rebuild(commit) {
+    sh.pushd('./TypeScript')
+    // checkout commit
+    sh.exec('git checkout ' + commit)
+    // re-build
+    sh.exec('npm install')
+    sh.exec('jake clean')
+    sh.exec('jake')
+    sh.popd()
+    console.log(first)
+    return compile()
+}
+// compile and get+count errors
+function compile() {
+    let ts = require('./TypeScript/built/local/typescript')
+    const counts = []
+    for (const repo of repos) {
+        console.log('Compiling ' + repo)
+        const config = ts.parseJsonSourceFileConfigFileContent(
+            ts.readJsonConfigFile(`/home/nathansa/ts/tests/cases/user/${repo}/tsconfig.json`,
+                                  fn => fs.readFileSync(fn, { encoding: "utf-8" })),
+            ts.sys,
+            `/home/nathansa/ts/tests/cases/user/${repo}/`)
+        const errors = ts.getPreEmitDiagnostics(ts.createProgram(config.fileNames, config.options))
+        counts.push({ repo, count: errors.length })
+    }
+    return counts
+    // for (const err of errors) {
+    //     console.log(err.messageText)
+    // }
+}
+
+// read json
+// reverse array to build better
+/** @type {Pr[]} */
+const prs = JSON.parse(fs.readFileSync('/home/nathansa/src/measure/prs.json', { encoding: "utf-8" })).reverse()
+/** @type {string[]} */
+const repos = JSON.parse(fs.readFileSync('/home/nathansa/src/measure/repos.json', { encoding: "utf-8" }))
+// TODO: Do this for all PRs and write out a new json file
+const first = prs[0]
+const errorCounts = rebuild(first.sha)
+const parentErrorCounts = rebuild(first.parentSha)
+console.log(`Difference: ${parentErrorCounts[1].count - errorCounts[1].count}`);
+
+// checkout parent commit
+// compiler and get+count errors
+// output difference in errors
+
