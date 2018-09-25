9e6c949410773d28c43842b73c31728322091917
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-09-25 13:13:12 -0700

    Refactor common code
    This includes nearly all the code.

--- anys.js
+++ anys.js
@@ -1,33 +1,24 @@
-const fs = require('fs')
-const sh = require('shelljs')
 const measure = require('./measure')
-// read json
-/** @type {Pr[]} NOTE: Reverse array to build better. */
-const prs = JSON.parse(fs.readFileSync('/home/nathansa/src/measure/prs.json', { encoding: "utf-8" })).reverse()
-/** @type {string[]} */
-const repos = JSON.parse(fs.readFileSync('/home/nathansa/src/measure/repos.json', { encoding: "utf-8" }))
+measure(
+    '/home/nathansa/src/measure/anys.json',
+    '/home/nathansa/src/measure/prs.json',
+    '/home/nathansa/src/measure/repos.json',
+    (ts, program) => {
+        let count = 0
+        const checker = program.getTypeChecker()
+        const privateTs = /** @type {*} I KNOW what I'm doing */(ts)
+        /**
+         * @param {import('./TypeScript/built/local/typescript').Node} node
+         * @return {void}
+         */
+        const walk = function (node) {
 
-/** @type {(ts: typeof import('typescript'),
-            program: import('typescript').Program) => number | null} */
-function test(ts, program) {
-    let errors = []
-    let count = 0
-    const checker = program.getTypeChecker()
-    const privateTs = /** @type {*} I KNOW what I'm doing */(ts)
-    /**
-
-     * @param {import('typescript').Node} node
-     * @return {void}
-     */
-    const walk = function (node) {
-
-        if ((privateTs.isExpressionNode(node) || node.kind === ts.SyntaxKind.Identifier || privateTs.isDeclarationName(node)) &&
-            checker.getTypeAtLocation(node) === /** @type {*} */(checker).getAnyType()) {
-            count++
+            if ((privateTs.isExpressionNode(node) || node.kind === ts.SyntaxKind.Identifier || privateTs.isDeclarationName(node)) &&
+                checker.getTypeAtLocation(node) === /** @type {*} */(checker).getAnyType()) {
+                count++
+            }
+            return ts.forEachChild(node, walk)
         }
-        return ts.forEachChild(node, walk)
-    }
-    try {
         for (const file of program.getRootFileNames()) {
             const sourceFile = program.getSourceFile(file)
             if (sourceFile) {
@@ -37,62 +28,5 @@ function test(ts, program) {
                 console.log("couldn't find " + file)
             }
         }
-    }
-    catch (ex) {
-        console.log('         failed to compile:')
-        console.log(ex)
-        return null
-    }
-    return count
-}
-
-const ts = require('typescript')
-const config = ts.parseJsonSourceFileConfigFileContent(
-    ts.readJsonConfigFile(`/home/nathansa/ts/tests/cases/user/async/tsconfig.json`,
-                          fn => fs.readFileSync(fn, { encoding: "utf-8" })),
-    ts.sys,
-    `/home/nathansa/ts/tests/cases/user/async/`)
-const program = ts.createProgram(config.fileNames, config.options)
-console.log(test(ts, program))
-
-// /** @param {string} commit */
-// function countErrors(commit) {
-//     measure.rebuild(commit)
-//     return measure.compile(repos, (ts, program) => {
-//         let errors = []
-//         try {
-//             errors = ts.getPreEmitDiagnostics(program)
-//         }
-//         catch (ex) {
-//             console.log('failed to compile:')
-//             console.log(ex)
-//             return null
-//         }
-//         return errors.length
-//     });
-// }
-// /**
-//  * @param {Count[]} previous
-//  */
-// function main(previous) {
-//     const alreadyRun = previous.length
-//     const errors = previous
-//     let i = 0
-//     for (const pr of prs) {
-//         i++
-//         if (i <= alreadyRun) {
-//             console.log(`Already ran ${pr.number} (${i}), skipping.`)
-//             continue
-//         }
-//         errors.push({
-//             date: pr.date,
-//             number: pr.number,
-//             before: { sha: pr.sha, repos: countErrors(pr.sha) },
-//             after: { sha: pr.parentSha, repos: countErrors(pr.parentSha) }
-//         })
-//         console.log(`*** DONE: ${i} of ${prs.length} **************`)
-//         fs.writeFileSync('errors.json', JSON.stringify(errors))
-//     }
-// }
-
-// main(JSON.parse(fs.readFileSync('/home/nathansa/src/measure/errors.json', { encoding: "utf-8" })))
+        return count
+    })
--- measure.js
+++ measure.js
@@ -38,11 +38,13 @@ function compile(repos, getCount) {
     const counts = []
     for (const repo of repos) {
         process.stdout.write(' - ' + repo)
+        const path = fs.existsSync(`/home/nathansa/ts/tests/cases/user/${repo}/tsconfig.json`) ?
+            `/home/nathansa/ts/tests/cases/user/${repo}/` :
+            `/home/nathansa/ts/tests/cases/user/${repo}/${repo}/`
         const config = ts.parseJsonSourceFileConfigFileContent(
-            ts.readJsonConfigFile(`/home/nathansa/ts/tests/cases/user/${repo}/tsconfig.json`,
-                                  fn => fs.readFileSync(fn, { encoding: "utf-8" })),
+            ts.readJsonConfigFile(path + 'tsconfig.json', fn => fs.readFileSync(fn, { encoding: "utf-8" })),
             ts.sys,
-            `/home/nathansa/ts/tests/cases/user/${repo}/`)
+            path)
         const program = ts.createProgram(config.fileNames, config.options)
         const count = getCount(ts, program)
         console.log(`  (${count})`)
@@ -51,8 +53,77 @@ function compile(repos, getCount) {
     return counts
 }
 
-
-module.exports = {
-    rebuild,
-    compile,
+/** @param {string} path */
+function read(path) {
+    return JSON.parse(fs.readFileSync(path, { encoding: "utf-8" }))
 }
+
+/**
+ * @param {string} commit
+ * @param {(ts: typeof import('./TypeScript/built/local/typescript'),
+            program: import('./TypeScript/built/local/typescript').Program) => number} getCount
+ * @param {string[]} repos
+ * @param {number} i - HACK: Skip async from PRs 22-27 when async never completed
+ */
+function count(commit, getCount, repos, i) {
+    rebuild(commit)
+    return compile(repos, (ts, program) => {
+        if (program.getRootFileNames()[0].startsWith('/home/nathansa/ts/tests/cases/user/async') && 22 <= i && i <= 27) return null
+        /** @type {number | null} */
+        let start = Date.now()
+        try {
+            return getCount(ts, program)
+        }
+        catch (ex) {
+            console.log(' failed to compile:')
+            console.log(ex)
+            return null
+        }
+    });
+}
+
+/**
+ * @param {string} previousPath
+ * @param {string} prPath
+ * @param {string} repoPath
+ * @param {(ts: typeof import('./TypeScript/built/local/typescript'),
+            program: import('./TypeScript/built/local/typescript').Program) => number} getCount
+ */
+function run(previousPath, prPath, repoPath, getCount) {
+    /** @type {Pr[]} */
+    const prs = read(prPath).reverse()
+    /** @type {string[]} */
+    const repos = read(repoPath)
+    /** @type {Count[]} */
+    const previous = read(previousPath)
+    const alreadyRun = previous.length
+    const errors = previous
+    let i = 0
+    for (const pr of prs) {
+        i++
+        if (i <= alreadyRun) {
+            console.log(`Already ran ${pr.number} (${i}), skipping.`)
+            continue
+        }
+        if (i === 39) {
+            console.log(`PR 39 doesn't build, skipping.`)
+            errors.push({
+                date: pr.date,
+                number: pr.number,
+                after: { sha: pr.parentSha, repos: repos.map(repo => ({ repo, count: null })) },
+                before: { sha: pr.sha, repos: repos.map(repo => ({ repo, count: null })) },
+            })
+            continue
+        }
+        errors.push({
+            date: pr.date,
+            number: pr.number,
+            after: { sha: pr.parentSha, repos: count(pr.parentSha, getCount, repos, i) },
+            before: { sha: pr.sha, repos: count(pr.sha, getCount, repos, i) },
+        })
+        console.log(`*** DONE: ${i} of ${prs.length} ***************************************************`)
+        fs.writeFileSync('errors.json', JSON.stringify(errors))
+    }
+}
+
+module.exports = run
