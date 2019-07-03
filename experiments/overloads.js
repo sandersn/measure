d66b5dee51ce88c76d8adabf0d810b3531ad4422
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-07-03 10:58:27 -0700

    Update overloads.js with newer code from dt-perf

--- experiments/overloads.js
+++ experiments/overloads.js
@@ -1,16 +1,30 @@
 const readline = require('readline')
 const sh = require('shelljs')
-const dtPath = "../../DefinitelyTyped/types"
+const path = require('path')
+const dtPath = path.resolve("../../../DefinitelyTyped/types/")
 const fs = require('fs')
 const process = require('process')
 const ts = require('typescript')
 
 
 const histogram = [0, 0, 0, 0, 0, 0]
+const deps = new Set()
 
 process.chdir(dtPath)
 for (const dir of fs.readdirSync(dtPath)) {
     for (const f of sh.find(dir).filter(f => f.endsWith('.d.ts'))) {
+        // skip repeated dependencies
+        // 1. because it's slow (aws-sdk specifically)
+        // 2. because it makes the counts wrong
+        const isdep = f.match(/\/node_modules\/(.+)/)
+        if (isdep) {
+            if (deps.has(isdep[1])) {
+                continue
+            }
+            else {
+                deps.add(isdep[1])
+            }
+        }
         const program = ts.createProgram([f], { types: [] })
         for (const f of program.getSourceFiles()) {
             if (f && f.fileName.endsWith('.d.ts') && !/lib.+\.d\.ts/.test(f.fileName) && f.fileName.indexOf('DefinitelyTyped/node_modules/@types/node/') === -1) {
@@ -18,7 +32,7 @@ for (const dir of fs.readdirSync(dtPath)) {
             }
             readline.clearLine(process.stdout, /*left*/ -1)
             readline.cursorTo(process.stdout, 0)
-            process.stdout.write('[' + histogram.map((n, i) => i + ': ' + n).join(', ') + ']\t' + f.fileName)
+            process.stdout.write('[' + histogram.map((n, i) => i + ':' + n).join(',') + ']\t' + f.fileName)
         }
     }
 }
@@ -48,8 +62,8 @@ function groupOverloads(parent) {
  * @returns {void}
  */
 function walk(node) {
-        for (const overload of groupOverloads(node)) {
-            histogram[overload.length] = (histogram[overload.length] || 0) + 1
+        for (const overloadset of groupOverloads(node)) {
+            histogram[overloadset.length] = (histogram[overloadset.length] || 0) + 1
         }
     return ts.forEachChild(node, walk)
 }
