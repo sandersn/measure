82b71b36b81f12602efee6bb7ccb78375c6ecc6b
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2021-06-23 10:15:35 -0700

    more detail in typedefs.js

--- experiments/typedefs.js
+++ experiments/typedefs.js
@@ -1,26 +1,34 @@
 // for #43677 which fixes #43534
-// test text to test commit from mac
+
 const fs = require('fs')
-const tslines = fs.readFileSync('./ts-typedefs.txt', 'utf8').split('\n')
+// const tslines = fs.readFileSync('./ts-typedefs.txt', 'utf8').split('\n')
 const jslines = fs.readFileSync('./js-typedefs.txt', 'utf8').split('\n')
-console.log(tslines.length)
+// console.log(tslines.length)
 console.log(jslines.length)
-console.log(stats(tslines))
+const js = stats(jslines)
+console.log({ ...js, other: js.other.length })
+fs.writeFileSync('./typedefs.txt', js.other.join('\n'))
 // console.log(stats(jslines))
 /** @param {string[]} lines */
 function stats(lines) {
     let errors = 0
+    let variants = 0
     let singleTypedefs = 0
     let allTypedefs = 0
+    let protobuf = 0
     let other = []
     for (const line of lines) {
         const tags = parseTags(line)
-        if (tags.length === 0)
+        if (line.includes('protobuf'))
+            protobuf++
+        else if (tags.length === 0)
             errors++
         else if (tags.length === 1 && tags[0] === 'typedef')
             singleTypedefs++
         else if (tags.every(t => t === 'typedef'))
             allTypedefs++
+        else if (tags.every(t => t === 'typedef' || t === 'template' || t === 'deprecated' || t === 'memberOf' || t === 'memberof' || t === 'property' || t === 'summary' || t === 'desc' || t === 'description' || t === 'example'))
+            variants++
         else
             other.push(line)
     }
@@ -28,6 +36,8 @@ function stats(lines) {
         errors,
         singleTypedefs,
         allTypedefs,
+        variants,
+        protobuf,
         other
     }
 }
